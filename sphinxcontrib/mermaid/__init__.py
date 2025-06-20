fa588030b547cab340d4d390ac2a88846f161b34
Eymeric CHAUCHAT <chauchat.eymeric.r7@dc.tohoku.ac.jp>
2025-06-20 17:58:28 +0900

    nase again

--- sphinxcontrib/mermaid/__init__.py
+++ sphinxcontrib/mermaid/__init__.py
@@ -474,7 +474,7 @@ def install_js(
     if app.config.mermaid_init_js:
 
         # Add the mermaid import from link to the init-js
-        app.config.mermaid_init_js = f'import mermaid from "{_mermaid_js_url}";' #{app.config.mermaid_init_js}
+        app.config.mermaid_init_js = f'import mermaid from "{_mermaid_js_url}";{app.config.mermaid_init_js}'
         
         # If mermaid is local the init-call must be placed after `html_js_files` which has a priority of 800.
         priority = app.config.mermaid_init_js_priority if _mermaid_js_url is not None else 801
